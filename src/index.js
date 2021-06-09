fecfb28e64b8d878052d125a5f3050d947aebae6
KatKava <katryn.kav97@gmail.com>
2021-06-09 14:08:04 +0100

    added comments

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+//The service worker is a web API that helps you cache your assets and other files so that when the user is offline or on slow network, he/she can still see results on the screen, as such it helps to  build a better user experience. All about adding offline capabilities. 
 
 // importing packages and setting up environment for react 
 
